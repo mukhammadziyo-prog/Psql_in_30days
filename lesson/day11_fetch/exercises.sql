35019b025b83ab04e5176327d4f5e32fa0bee49b
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-19 11:14:32 +0500

    Add day11 FETCH lesson

---
+++ lesson/day11_fetch/exercises.sql
@@ -0,0 +1,11 @@
+-- Task 1
+Select the first 5 users.
+
+-- Task 2
+Select the first 10 users.
+
+-- Task 3
+Select the 3 oldest users.
+
+-- Task 4
+Select the first 2 users showing only first_name and email.
