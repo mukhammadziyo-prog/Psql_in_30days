636df65fc60e9253dcbb616e2d853ed1b309e9de
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-19 21:29:12 +0500

    solutions

---
+++ lesson/day12_aliases/solutions.sql
@@ -0,0 +1,14 @@
+SELECT first_name AS name
+FROM users;
+
+SELECT email AS user_email
+FROM users;
+
+SELECT age AS user_age
+FROM users;
+
+SELECT city AS location
+FROM users;
+
+SELECT u.first_name
+FROM users AS u;
