ae21941fc2d3bacddb679f815aa46f9070d46279
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-19 21:28:19 +0500

    you should perform this exercises

---
+++ lesson/day12_aliases/exercises.sql
@@ -0,0 +1,14 @@
+-- Task 1
+Display first_name as name.
+
+-- Task 2
+Display email as user_email.
+
+-- Task 3
+Display age as user_age.
+
+-- Task 4
+Display city as location.
+
+-- Task 5
+Use a table alias for users.
