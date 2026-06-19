4f3071d8f5a4685e55baf22cfbeb9e9879f9ea3c
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-19 21:34:39 +0500

    find out the solutions

---
+++ lesson/day13_AND/exercises.sql
@@ -0,0 +1,15 @@
+-- Task 1
+Select all users whose age is greater than 20
+and city is Tashkent.
+
+-- Task 2
+Select all users whose age is less than 30
+and city is Samarkand.
+
+-- Task 3
+Select first_name and email for users whose
+age is greater than 18 and city is Bukhara.
+
+-- Task 4
+Select all users whose first_name is 'Ali'
+and age is greater than 20.
