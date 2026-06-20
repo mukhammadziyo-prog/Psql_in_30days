1e2e97f16017a3e0df2bd387820700e601459ddb
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-20 21:22:37 +0500

    find out the solutions

---
+++ lesson/day15_in/exercises.sql
@@ -0,0 +1,11 @@
+-- Task 1
+Select all users from Tashkent or Samarkand.
+
+-- Task 2
+Select all users whose age is 18, 20, or 25.
+
+-- Task 3
+Select first_name and email for users from Bukhara or Khiva.
+
+-- Task 4
+Select all users whose first_name is Ali or Muhammad.
