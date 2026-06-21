39e2bd8787a7fc5e15d26851fcc4d297780ef757
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-21 12:54:52 +0500

    Add day 17 LIKE lesoon

---
+++ lesson/day17_like/solutions.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM users
+WHERE first_name LIKE 'A%';
+
+SELECT *
+FROM users
+WHERE email LIKE '%gmail.com';
+
+SELECT *
+FROM users
+WHERE city LIKE 'T%';
+
+SELECT *
+FROM users
+WHERE first_name LIKE '_l%';
