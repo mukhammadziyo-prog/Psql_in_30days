ac02043afc26b75c8a20b1708bfa4690dae8576d
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-21 12:54:19 +0500

    exercises

---
+++ lesson/day17_like/exercises.sql
@@ -0,0 +1,11 @@
+-- Task 1
+Select all users whose first_name starts with 'A'.
+
+-- Task 2
+Select all users whose email ends with 'gmail.com'.
+
+-- Task 3
+Select all users whose city starts with 'T'.
+
+-- Task 4
+Select all users whose first_name has 'l' as the second character.
