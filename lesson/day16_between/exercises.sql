2ac6402274a9770e9826b14e56d0d0129bd545fe
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-21 12:40:48 +0500

    find out the solutions

---
+++ lesson/day16_between/exercises.sql
@@ -0,0 +1,11 @@
+-- Task 1
+Select all users whose age is between 18 and 25.
+
+-- Task 2
+Select all users whose age is between 20 and 30.
+
+-- Task 3
+Select first_name and email for users whose age is between 18 and 22.
+
+-- Task 4
+Select all users born between 2000-01-01 and 2010-12-31.
