184f4897da02cd7ce08d86cbd294500230ff4c11
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-22 21:44:29 +0500

    changed seed.sql and added 15 users

--- database/seed.sql
+++ database/seed.sql
@@ -8,21 +8,8 @@ INSERT INTO users (
     city
 )
 VALUES
-(
-    'Muhammad',
-    'Niyatov',
-    'muhammad@example.com',
-    '2007-12-21',
-    '956784334',
-    19,
-    'Tashkent'
-),
-(
-    'Ali',
-    'Karimov',
-    'ali@example.com',
-    '2003-05-15',
-    '998901112233',
-    22,
-    'Samarkand'
-);
+('Ali','Karimov','ali@example.com','2003-05-15','998901112233',22,'Tashkent'),
+('Vali','Tursunov','vali@example.com','2002-03-10','998901112234',23,'Samarkand'),
+('Aziz','Rahimov','aziz@example.com','2005-08-22','998901112235',20,'Bukhara'),
+('Akmal','Karimov','akmal@example.com','2001-11-02','998901112236',24,'Tashkent'),
+('Jasur','Aliyev','jasur@example.com','2004-01-17','998901112237',21,'Khiva');
