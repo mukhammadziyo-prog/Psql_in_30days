5c6641b91a56c922487a68b9b1813b7fc73fb1b4
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-23 14:32:26 +0500

    find out the solutions

---
+++ lesson/day18_is_null/exercises.sql
@@ -0,0 +1,11 @@
+-- Task 1
+Select all users whose phone_number is NULL.
+
+-- Task 2
+Select all users whose phone_number is NOT NULL.
+
+-- Task 3
+Select all users whose email is NULL.
+
+-- Task 4
+Select all users whose birthday is NULL.
