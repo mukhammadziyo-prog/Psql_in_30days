32fec3beb9391d5f948fdbf8705bb11d2f4e754e
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-24 16:56:01 +0500

    examples file completed

---
+++ lesson/day20_min_max/examples.sql
@@ -0,0 +1,17 @@
+-- Task 1
+Find the youngest user's age.
+
+-- Task 2
+Find the oldest user's age.
+
+-- Task 3
+Find the earliest birthday.
+
+-- Task 4
+Find the latest birthday.
+
+-- Task 5
+Find the first first_name alphabetically.
+
+-- Task 6
+Find the last first_name alphabetically.
