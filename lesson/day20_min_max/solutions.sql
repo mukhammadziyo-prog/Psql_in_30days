2e30b3d3eae4f57526c3e7fbea59eba39a54b9df
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-24 16:59:29 +0500

    Add DAY 20 MIN_MAX lesson

---
+++ lesson/day20_min_max/solutions.sql
@@ -0,0 +1,17 @@
+SELECT MIN(age)
+FROM users;
+
+SELECT MAX(age)
+FROM users;
+
+SELECT MIN(birthday)
+FROM users;
+
+SELECT MAX(birthday)
+FROM users;
+
+SELECT MIN(first_name)
+FROM users;
+
+SELECT MAX(first_name)
+FROM users;
