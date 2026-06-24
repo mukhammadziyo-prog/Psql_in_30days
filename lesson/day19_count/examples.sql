b11bed4627989fe866b283f8a519336e41634490
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-24 16:11:59 +0500

    examples added

---
+++ lesson/day19_count/examples.sql
@@ -0,0 +1,16 @@
+SELECT COUNT(*)
+FROM users;
+
+SELECT COUNT(city)
+FROM users;
+
+SELECT COUNT(email)
+FROM users;
+
+SELECT COUNT(*)
+FROM users
+WHERE city = 'Tashkent';
+
+SELECT COUNT(*)
+FROM users
+WHERE age > 20;
