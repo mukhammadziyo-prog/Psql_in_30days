5cbf8e7ccbfcf8f7017cd0ea9020a96c9f0d8c98
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-25 22:13:38 +0500

    find out the solutions

---
+++ lesson/day22_group_by/exercises.sql
@@ -0,0 +1,14 @@
+-- Task 1
+Count users in each city.
+
+-- Task 2
+Calculate the average age in each city.
+
+-- Task 3
+Find the oldest age in each city.
+
+-- Task 4
+Find the youngest age in each city.
+
+-- Task 5
+Calculate the total age in each city.
