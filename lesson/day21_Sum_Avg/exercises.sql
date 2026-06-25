7998621c0c4d6664623f130052dacc65135cfa95
Fajita Felecia <fajitafe@ji-f1.tash.21-school.uz>
2026-06-25 13:19:27 +0500

    find out the solutions

---
+++ lesson/day21_Sum_Avg/exercises.sql
@@ -0,0 +1,11 @@
+-- Task 1
+Calculate the to total age of all users.
+
+-- Task 2
+Calculate the average age of all users.
+
+-- Task 3
+Calculate the total age of users from Tashkent.
+
+-- Task 4
+Calculate the average age of users from Samarkand.
