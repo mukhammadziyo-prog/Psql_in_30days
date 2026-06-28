a783ef0c988f29df55572f8eb508db3c55eb99ae
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-06-28 23:48:19 +0500

    find out the solutions through using psql commands

---
+++ lesson/day23_having/exercises.sql
@@ -0,0 +1,11 @@
+-- Task 1
+Show cities that have more than 2 users.
+
+-- Task 2
+Show cities where the average age is greater than 22.
+
+-- Task 3
+Show cities where the oldest user is at least 25 years old.
+
+-- Task 4
+Show cities where the total age of users is greater than 60.
