42c37bd8e6af1d0c732464fd89c32eff3f0192b5
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-07-03 20:09:44 +0500

    exercises.sql completed

---
+++ lesson/day27_full_join/exercises.sql
@@ -0,0 +1,8 @@
+-- Task 1
+Use FULL JOIN to display every user and every order.
+
+-- Task 2
+Display first_name and total_amount using FULL JOIN.
+
+-- Task 3
+Display first_name and order_date using FULL JOIN.
