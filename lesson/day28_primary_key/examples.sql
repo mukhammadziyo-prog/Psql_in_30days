c69b8ebd47fb4682cd4f90098b65bcbaa1906c85
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-07-04 22:22:08 +0500

    updated

--- lesson/day28_primary_key/examples.sql
+++ lesson/day28_primary_key/examples.sql
@@ -1,9 +1,1 @@
-SELECT users.first_name, orders.total_amount
-FROM users
-FULL JOIN orders
-ON users.id = orders.user_id;
-
-SELECT users.first_name, orders.order_date
-FROM users
-FULL JOIN orders
-ON users.id = orders.user_id;
+k
