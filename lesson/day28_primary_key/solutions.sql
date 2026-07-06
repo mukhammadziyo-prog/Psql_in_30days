6dbec02bf097714f93ef46806d1258c92c82db36
Mukhammadziyo <mukhammadziyomn@gmail.com>
2026-07-06 23:51:26 +0500

    Add day 28 PRIMARY KEY lesson

--- lesson/day28_primary_key/solutions.sql
+++ lesson/day28_primary_key/solutions.sql
@@ -1,1 +1,17 @@
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    age INT
+);
 
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100),
+    author VARCHAR(100)
+);
+
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+);
