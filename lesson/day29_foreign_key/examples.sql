b90e53f50f5cfaac15bfb72be9cfe01e41d5c627
Mukhammadziyo <244293343+mukhammadziyo-prog@users.noreply.github.com>
2026-07-23 17:27:02 +0500

    examples

---
+++ lesson/day29_foreign_key/examples.sql
@@ -0,0 +1,17 @@
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    order_date DATE,
+    total_amount DECIMAL(10,2),
+
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+);
+
+
+SELECT *
+FROM orders;
+
+
+SELECT *
+FROM users;
